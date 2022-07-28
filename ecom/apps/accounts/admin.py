4bc034c314fb4e38a1b576c8e357dba7bb64e4cb
shnt007 <sushantd34@gmail.com>
2022-07-28 13:56:02 +0545

    Admin Page Setup and Modified

--- ecom/apps/accounts/admin.py
+++ ecom/apps/accounts/admin.py
@@ -4,3 +4,13 @@ from .models import Profile
 
 # Register your models here.
 admin.site.register(Profile)
+
+admin.site.site_header = "E-Com"
+admin.site.site_title = "Admin"
+admin.site.index_title = "E-Com"
+
+
+class Profile(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('frist_name', 'email')
+    list_filter = ('first_name', 'last_name', 'email')
